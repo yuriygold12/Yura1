cc350fb477ef2f4571399d8a7eb0aac009f09bf9
yuriygold12 <yra.bahlay@gmail.com>
2017-12-11 13:20:02 +0200

    Start Task 0301

---
+++ src/Home/RunnerOrder.java
@@ -0,0 +1,9 @@
+package Home;
+
+public class RunnerOrder {
+
+    public static void main(String[] args) {
+        Order Telefon = new Order("Yuriy", "Bahlay", "Antonuca 20/123",
+                2223455, "yra.bahlay@gmail.com");
+    }
+}
