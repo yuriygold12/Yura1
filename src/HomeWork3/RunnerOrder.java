0bd3aed9661998b414d6c7b35296cd90315ac32f
yuriygold12 <yra.bahlay@gmail.com>
2017-12-30 14:14:32 +0200

    Meeting 8

---
+++ src/HomeWork3/RunnerOrder.java
@@ -0,0 +1,8 @@
+package HomeWork3;
+
+public class RunnerOrder {
+    public static void main(String[] args) {
+        Customer Telefon = new Customer("Yuriy", "Bahlay", "Antonuca 20/123",
+                2223455, "yra.bahlay@gmail.com");
+    }
+}
