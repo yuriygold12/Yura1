1940ae8e47a6af8bd0288b8e868dbca79493e978
yuriygold12 <yra.bahlay@gmail.com>
2018-03-14 10:45:42 +0200

    ExeptionArray

--- src/HomeWork23/ArrayNegativeSizeExeption.java
+++ src/HomeWork23/ArrayNegativeSizeExeption.java
@@ -12,16 +12,15 @@ public class ArrayNegativeSizeExeption {
                 System.out.println("Введіть size елементыв масиву: ");
                 size = sc.nextInt();
                 int[] ar = new int[size];
-
-            } catch(InputMismatchException e) {
+                break;
+            } catch (InputMismatchException e) {
                 System.out.println("Введено недопустимий символ :" + e.getMessage());
                 sc.nextLine();
-            }
-            catch (NegativeArraySizeException e) {
-                System.out.println("Результат : "+ e.getMessage());
+            } catch (NegativeArraySizeException e) {
+                System.out.println("Результат : " + e.getMessage());
                 System.out.println("Не вірно введено кількість елементів у масив");
             }
-        }while(size <= 0);
-        System.out.println("Кількість елементів масиву : "+size);
+        } while (true);
+        System.out.println("Кількість елементів масиву : " + size);
     }
 }
