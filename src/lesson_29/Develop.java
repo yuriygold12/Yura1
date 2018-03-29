f3d0ef9e29fa6f7801ee84b0e8cbd4659a0fcee6
yuriygold12 <yra.bahlay@gmail.com>
2018-03-29 18:24:02 +0300

    PatternuGOFProduct1

---
+++ src/lesson_29/Develop.java
@@ -0,0 +1,10 @@
+package lesson_29;
+
+public interface Develop {
+    public void analisyTask();
+    public void codeTask();
+    public void testTask();
+    public void refectTask();
+}
+
+
---
+++ src/lesson_29/JavaDeveloperHire.java
@@ -0,0 +1,8 @@
+package lesson_29;
+
+public class JavaDeveloperHire extends DevelopProject {
+    @Override
+    public Develop hireDeveloper() {
+        return new JavaDeveloper();
+    }
+}
