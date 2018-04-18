db4dc0b4294b9101590dd0bdbd41cba25b31ed94
yuriygold12 <yra.bahlay@gmail.com>
2018-04-18 12:12:20 +0300

    Patert metod fabric

--- src/lesson_28_1/Creater.java
+++ src/lesson_28_1/Creater.java
@@ -2,4 +2,8 @@ package lesson_28_1;
 
 public abstract class Creater {
     public abstract Product createrProduct();
+    public void RealisationProduct() {
+        Product product = createrProduct();
+        product.dosometing();
+    }
 }
